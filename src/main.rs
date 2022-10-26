a34176c179cf5f5e6e747fa85c245d9f7d58b414
Asir Zaki <asir.zaki.v2@gmail.com>
2022-10-25 21:01:09 -0400

    Basic in-out print statements and user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+//use std::io::{Write, BufReader,  BufRead, ErrorKind};
+//use rand::Rng;
+//use std::fs::File;
+//use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What's your name?");
+    //mut keyword: mutable variable (default immutable so this needs to be specific)
+    let mut name: String = String::new();
+    let greeting: &str = "This is a greeting message";
+    let name_acknowledgement: &str = "So your name is";
+
+    io::stdin().read_line(&mut name).expect("no input?");
+    println!("{}! {} {}.", greeting, name_acknowledgement, name.trim_end());
 }
